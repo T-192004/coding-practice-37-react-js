f572f657403ed7c65cbd137c618e17c3d718335c
Tanvi Tomar <128494418+T-192004@users.noreply.github.com>
2024-05-15 19:41:26 +0530

    Update index.js

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -13,31 +13,35 @@ const VaccinationByAge = props => {
   const {vaccinationByAge} = props
   console.log(vaccinationByAge)
   return (
-    <ResponsiveContainer width="100%" height={300} align="center">
-      <PieChart>
-        <Pie
-          cx="70%"
-          cy="40%"
-          data={vaccinationByAge}
-          startAngle={0}
-          endAngle={360}
-          outerRadius="70%"
-          dataKey="count"
-        >
-          <Cell name={vaccinationByAge[0].age} fill="#5a8dee" />
-          <Cell name={vaccinationByAge[1].age} fill="#2cc6c6" />
-          <Cell name={vaccinationByAge[2].age} fill="#a3df9f" />
-        </Pie>
-        <Legend
-          iconType="Circle"
-          layout="horizontal"
-          horizontalAlign="middle"
-          verticalAlign="bottom"
-          align="center"
-        />
-      </PieChart>
-    </ResponsiveContainer>
+    <>
+      <PieChart
+        width={100}
+        height={100}
+        cx="70%"
+        cy="40%"
+        data={vaccinationByAge}
+        startAngle={0}
+        endAngle={360}
+        outerRadius="70%"
+        dataKey="count"
+      />
+      <Cell name={vaccinationByAge[0].age} fill="#5a8dee" />
+      <Cell name={vaccinationByAge[1].age} fill="#2cc6c6" />
+      <Cell name={vaccinationByAge[2].age} fill="#a3df9f" />
+      <Legend
+        iconType="Circle"
+        layout="horizontal"
+        horizontalAlign="middle"
+        verticalAlign="bottom"
+        align="center"
+      />
+    </>
   )
 }
 
 export default VaccinationByAge
+
+//
+//     </Pie>
+//
+//   </PieChart>
