a1f322384136cc12d4a8d4470e2693409c3078ba
Tanvi Tomar <128494418+T-192004@users.noreply.github.com>
2024-05-16 08:05:06 +0530

    Update index.js

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -13,8 +13,8 @@ const VaccinationByAge = props => {
   const {vaccinationByAge} = props
   console.log(vaccinationByAge)
   return (
-    <>
-      <PieChart
+    <PieChart width={1000} height={500}>
+      <Pie
         width={100}
         height={100}
         cx="70%"
@@ -28,6 +28,7 @@ const VaccinationByAge = props => {
       <Cell name={vaccinationByAge[0].age} fill="#5a8dee" />
       <Cell name={vaccinationByAge[1].age} fill="#2cc6c6" />
       <Cell name={vaccinationByAge[2].age} fill="#a3df9f" />
+  </Pie>
       <Legend
         iconType="Circle"
         layout="horizontal"
@@ -35,7 +36,8 @@ const VaccinationByAge = props => {
         verticalAlign="bottom"
         align="center"
       />
-    </>
+          
+    </PieChart>
   )
 }
 
